5ef0521b9cff8db9bf7747ffd5f6010c1e1dc1ce
kiwibatave <b_cuzon@yahoo.fr>
2017-10-11 14:28:03 +0200

    fourth commit tuto start controleurs

--- src/BC/PlatformBundle/Controller/AdvertController.php
+++ src/BC/PlatformBundle/Controller/AdvertController.php
@@ -11,18 +11,20 @@ namespace BC\PlatformBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 // le nom de notre contrôleur respecte le nom du fichier pour que l'autoload fonctionne.
 use Symfony\Component\HttpFoundation\Response;
+//Ce use est utilisé pour générer l'url complète de chaque page
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class AdvertController extends Controller
 {
-    public function viewAction()
+    public function viewAction($id)
     {
-        //On souhaite avoir l'url de l'annonce d'id par exemple 5.
-        $url = $this->get('router')->generate(
-            'bc_platform_view', // On passe le 1er argument : le nom de la route
-            array('id' => 5)
-        );
-        //$url vaut maintenant /platform/advert/5
-        return new Response("L'URL de l'annonce d'id 5 est : ".$url);
+        // $id vaut 5 si l'on a appelé l'URL /platform/advert/5
+        // Ici, on récupèrera depuis la base de données
+        // l'annonce correspondant à l'id $id.
+        // Puis on passera l'annonce à la vue pour
+        // qu'elle puisse l'afficher
+
+        return new Response("Affichage de l'annonce d'id: ".$id);
     }
 
     public function viewSlugAction($slug, $year, $format)
@@ -31,17 +33,25 @@ class AdvertController extends Controller
             "On pourrait afficher l'annonce correspondant au slug '".$slug."', créée en ".$year." et au format ".$format."."
         );
     }
-//    public function indexAction()
-////On définit la méthode indexAction(). Mettre le suffixe Action derrière le nom de la méthode.
-//    {
-////On récupère via GET le template et on l'affiche via RENDER en allant le chercher où il se trouve
-//        $content = $this
-//            ->get('templating')
-//            ->render('BCPlatformBundle:Advert:index.html.twig', array('name' => 'Hombres'))
-//        ;
-////On crée une simple réponse. L'argument de l'objet Response. Et on retourne l'objet.
-//        return new Response($content);
-//    }
+
+    public function indexAction()
+    {
+        $url = $this
+            ->get('router')
+            ->generate(
+                'bc_platform_view',
+                array('id' => 5)
+            );
+        return new Response("L'URL de l'annonce d'id 5 est : ".$url);
+//  Cette fonction permet de généner l'url en entière de chaque page
+//        $url = $this
+//            ->get('router')
+//            ->generate(
+//                'oc_platform_home',
+//                array(), UrlGeneratorInterface::ABSOLUTE_URL
+//            );
+    }
+
     public function endAction()
     {
         $lastpage = $this
