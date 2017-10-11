0402a7ce5b31fac9077f02ef666cf32c22897b4d
kiwibatave <b_cuzon@yahoo.fr>
2017-10-11 14:57:35 +0200

    five tuto manip objet response

--- src/BC/PlatformBundle/Controller/AdvertController.php
+++ src/BC/PlatformBundle/Controller/AdvertController.php
@@ -10,21 +10,26 @@ namespace BC\PlatformBundle\Controller;
 //use pour utiliser notre bundle Platform
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 // le nom de notre contrôleur respecte le nom du fichier pour que l'autoload fonctionne.
+use Symfony\Component\HttpFoundation\Request;
+//Use utilisé pour récupérer le Request
 use Symfony\Component\HttpFoundation\Response;
 //Ce use est utilisé pour générer l'url complète de chaque page
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+//use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class AdvertController extends Controller
 {
-    public function viewAction($id)
+    //On injecte la requête dans les arguments de la méthode
+    public function viewAction($id, Request $request)
     {
+        // On récupère le paramètre tag
+        $tag = $request->query->get('tag');
         // $id vaut 5 si l'on a appelé l'URL /platform/advert/5
         // Ici, on récupèrera depuis la base de données
         // l'annonce correspondant à l'id $id.
         // Puis on passera l'annonce à la vue pour
         // qu'elle puisse l'afficher
 
-        return new Response("Affichage de l'annonce d'id: ".$id);
+        return new Response("Affichage de l'annonce d'id: ".$id.", avec le tag : ".$tag);
     }
 
     public function viewSlugAction($slug, $year, $format)
